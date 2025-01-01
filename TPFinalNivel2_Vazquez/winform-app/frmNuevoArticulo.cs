01c23dece3b252a8f0e4bdeadaeba007e50de8c2
Ian Vázquez <cam.iva@hotmail.com>
2025-01-01 17:24:12 -0300

    Se logra modificar el artículo sin los ComboBox

--- TPFinalNivel2_Vazquez/winform-app/frmNuevoArticulo.cs
+++ TPFinalNivel2_Vazquez/winform-app/frmNuevoArticulo.cs
@@ -15,12 +15,22 @@ namespace winform_app
 {
     public partial class frmNuevoArticulo : Form
     {
-        Articulo articulo = new Articulo();
+        private Articulo articulo = null;
         ArticuloNegocio articuloNegocio = new ArticuloNegocio();
 
+        // Constructor para el alta
         public frmNuevoArticulo()
         {
             InitializeComponent();
+        }
+
+        // Constructor para la modificación
+        public frmNuevoArticulo(Articulo articulo)
+        {
+            InitializeComponent();
+            this.articulo = articulo;
+            Text = "Modificar artículo";
+            lblTitulo.Text = "Modificar artículo";
         }
 
         private void frmNuevoArticulo_Load(object sender, EventArgs e)
@@ -31,18 +41,43 @@ namespace winform_app
             try
             {
                 cmbCategoria.DataSource = categoriaNegocio.listar();
-                cmbMarca.DataSource = marcaNegocio.listar();
+                cmbCategoria.ValueMember = "Id";
+                cmbCategoria.DisplayMember = "Descripcion";
 
-                cmbCategoria.SelectedIndex = -1;
-                cmbMarca.SelectedIndex = -1;
+                cmbMarca.DataSource = marcaNegocio.listar();
+                cmbMarca.ValueMember = "Id";
+                cmbMarca.DisplayMember = "Descripcion";
+
+                // Los ComboBox se inicializan sin selección
+                //cmbCategoria.SelectedIndex = -1;
+                //cmbMarca.SelectedIndex = -1;
+
+                // Precargamos los datos para el form de actualización
+                if (articulo != null) {
+                    txtCodigo.Text = articulo.Codigo;
+                    txtNombre.Text = articulo.Nombre;
+                    txtDescripcion.Text = articulo.Descripcion;
+
+                    txtImagen.Text = articulo.ImagenUrl;
+                    cargarImagen(articulo.ImagenUrl);
+
+                    cmbCategoria.SelectedValue = articulo.IdCategoria.Id;
+                    cmbMarca.SelectedValue = articulo.IdMarca.Id;
+                    txtPrecio.Text = articulo.Precio.ToString();
+                }
+
             }
-            catch (Exception ex) { throw ex; }
+            catch (Exception ex) { MessageBox.Show("Error al cargar los ComboBox", ex.Message); }
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             try
             {
+                if (articulo == null){
+                    articulo = new Articulo();
+                }
+
                 articulo.Codigo = txtCodigo.Text;
                 articulo.Nombre = txtNombre.Text;
                 articulo.Descripcion = txtDescripcion.Text;
@@ -51,8 +86,22 @@ namespace winform_app
                 articulo.IdMarca = (Marca)cmbMarca.SelectedItem;
                 articulo.Precio = decimal.Parse(txtPrecio.Text);
 
-                articuloNegocio.agregarArticulo(articulo);
-                MessageBox.Show(articulo.Nombre + " agregado exitosamente", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (articulo.Id != 0)
+                {
+                    articuloNegocio.modificarArticulo(articulo);
+                    MessageBox.Show(articulo.Nombre + " modificado exitosamente", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Los ComboBox se inicializan sin selección
+                    cmbCategoria.SelectedIndex = -1;
+                    cmbMarca.SelectedIndex = -1;
+
+                    articuloNegocio.agregarArticulo(articulo);
+                    MessageBox.Show(articulo.Nombre + " agregado exitosamente", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
 
                 this.Close();
             }
